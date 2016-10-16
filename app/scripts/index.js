d608006ec8ca2a79b0d9d094661ac96c1b6d8498
Matias Mariani <matiasironyard@gmail.com>
2016-10-16 17:55:47 -0400

    finished for now

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -35,21 +35,7 @@ $(document).on('villain:selected', function(event, villain){
   selectedVillain = villain;
 });
 
-
-// $(document).on('attack:villain', function(event){
-//   var damage = Math.floor(Math.random()* 10);
-//   // this.health = this.health - damage;
-//   selectedVillain.health = selectedVillain.health - damage;
-//
-//   $(document).trigger('health:change');
-//
-//   console.log(selectedHero);
-//   selectedHero.attack(selectedVillain);
-// });
-
-
-
-  $('img').on('click', function(event){
+$('img').on('click', function(event){
     event.preventDefault();
 
     var $heroSelect = $(this);
@@ -59,48 +45,41 @@ $(document).on('villain:selected', function(event, villain){
     selectedHero = _.filter(heroes, {'name': heroName})[0];
 
     $(this).parents().siblings('.hero-info').hide();
-
-
     //Villain selection and html attributes
     selectedVillain = villains[randomVillain];
     $('.villain-name').html(selectedVillain.name);
     $('#villain-pic').attr("src", selectedVillain.image);
-    console.log(selectedVillain.image);
-
+    // console.log(selectedVillain.image);
     console.log(selectedHero);
     console.log(selectedVillain);
-  });
-
-
-
+});
 
   $('.fire-button').click(function(event){
-    // console.log(selectedHero);
-
     event.preventDefault();
-
     window.setTimeout(function(){
       selectedVillain.attack(selectedHero);
       // $('.hero-health').html(this.health); dont delete
       if ($('.hero-health').html !== selectedHero.health) {
         $('.hero-health').html(selectedHero.health);
       }
+      $('#gun-shot').get(0).play();
       console.log("hero health" +selectedHero.health);
-    }, 2000);
+    }, 4000);
 
-
+    if(selectedHero.health <= 0){
+      $('.message').html("You Lose!");
+    }
   });
-  $('.fire-button').click(function(event){
-    // console.log(selectedHero);
-    event.preventDefault();
-    selectedHero.attack(selectedVillain);  // $('.hero-health').html(this.health); dont delete
 
-      // if($('.villain-health').html == selectedVillain.health) {
-      //     $('.villain-health').html(selectedVillain.health);
-          console.log("test" + selectedVillain.health);
-      //  }
-      if(selectedHero.health <= 0){
-        $('.message').html( "You Lose");
+  $('.fire-button').click(function(event){
+      $('#gun-shot').get(0).play();
+      event.preventDefault();
+      selectedHero.attack(selectedVillain);
+      $('.villain-health').html(selectedVillain.health);
+      console.log("fire at villain" + selectedVillain.health);
+
+      if(selectedVillain.health <= 0){
+        $('.message').html("You Win!");
       }
     });
 
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -5,27 +5,20 @@ function Character(config) {
   config = config || {};
   this.health = 50;
   _.extend(this, config);
-  // console.log(this.health);
-
 }
 
-//what do we pass in?
 Character.prototype.attack = function(hero){
   var damage = Math.floor(Math.random()* 10);
   this.health = this.health - damage;
   $(document).trigger('health:change');
   console.log('characters health' + this.health);
 };
-// Villain.prototype.villainDamage = function(hero){
-//   this.health = this.heath;
-//   console.log(this.health);
-// };
 
 function Hero(config) {
   // this.health = 50;
   Character.call(this, config);
-
 }
+
 Hero.prototype = new Character();
 
 function Villain(config) {
@@ -33,9 +26,7 @@ function Villain(config) {
   Character.call(this, config);
 }
 
-
 Villain.prototype = new Character();
-
 
 module.exports = {
   'Character': Character,
